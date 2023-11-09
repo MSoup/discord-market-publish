47b8289f1e12eee0c5bbfb47b61b0fd44bccc71c
MSoup-Slalom <daven.lu@slalom.com>
2023-11-09 13:28:54 +0900

    clean up error handling

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,1 @@
 type MarketSymbol = 'usd-jpy' | 'spy' | 'spy-futures';
-
-export interface MarketMetadata {
-  title: string;
-  description: string;
-  filename: string;
-}
--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,7 @@ export const isMarketOpen = (time: Date) =>
   time.getHours() < 5 ||
   (time.getHours() >= 22 && time.getMinutes() >= 30) ||
   time.getHours() >= 23;
+
+export const logError = (error: Error) => {
+  console.error(error);
+};
